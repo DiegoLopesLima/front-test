05d5e3618ce75acad978da936c0e3308a5202a11
DiegoLopesLima <web.diego.lima@gmail.com>
2020-01-22 00:11:06 -0300

    Configuração inicial de Webpack para Babel, TypeScript e Jest

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const
+  path = require('path'),
+
+  HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: [ '.ts', '.tsx', '.js', '.mjs' ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env'
+              ]
+            }
+          },
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      },
+      // {
+      //   test: /\.scss$/,
+      //   exclude: /node_modules/,
+      //   use: []
+      // }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+    hot: true,
+    historyApiFallback: {
+      rewrites: [
+        {
+          from: /.*/,
+          to: 'index.html'
+        }
+      ]
+    },
+    open: true
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: false
+      }
+    })
+  ]
+};
